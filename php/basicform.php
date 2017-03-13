a37293345c6de89b11ad25bc7de9f1189082c788
FASUS\Kadaiser <dvalbuen@ucm.es>
2017-03-13 13:21:14 +0100

    basicform.php added

--- php/basicform.php
+++ php/basicform.php
@@ -5,7 +5,13 @@
     <title>Response from Basic Form</title>
   </head>
   <body>
-    <?php
-    ?>
+    <?php echo "The server have process the follow information:"; ?>
+    <span>Mail:</span>
+    <?php echo $_POST["mail"]; ?><br><span>Password:</span>
+    <?php echo $_POST["pass"]; ?><br><span>Gender:</span>
+    <?php echo $_POST["gender"]; ?><br><span>Age:</span>
+    <?php echo $_POST["age"]; ?><br><span>Your favorite FF</span>
+    <?php echo $_POST["game"]; ?><br><span>Your opinion</span>
+    <?php echo $_POST["about"]; ?><br>
   </body>
 </html>
